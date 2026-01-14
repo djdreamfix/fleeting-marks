6333316135a70837d1e1a3fe298f71f953886bd9
djdreamfix <djdreamfix@gmail.com>
2026-01-14 17:02:29 +0200

    Implement socket.io for real-time marker updates
    Add socket.io integration for marker events

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -5,23 +6,51 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { io } from "socket.io-client";
+
+const socket = io("https://fleeting-marks.onrender.com");
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+
+  useEffect(() => {
+
+    socket.on("marker:created", (marker) => {
+      console.log("Нова мітка:", marker);
+      if (window.addMarkerToMap) {
+        window.addMarkerToMap(marker);
+      }
+    });
+
+    socket.on("marker:removed", (id) => {
+      console.log("Видалена мітка:", id);
+      if (window.removeMarkerFromMap) {
+        window.removeMarkerFromMap(id);
+      }
+    });
+
+    return () => {
+      socket.off("marker:created");
+      socket.off("marker:removed");
+    };
+
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
